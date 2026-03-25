7a279c444cac2836fad876f417ca94172c46d9a9
wtut-2025353 <wtut-2025353@kinal.edu.gt>
2026-03-25 10:53:56 -0600

    feat: creación de la entidad usuario

---
+++ src/main/java/com/wilsontut/kinalapp/entity/Usuario.java
@@ -0,0 +1,89 @@
+package com.wilsontut.kinalapp.entity;
+ 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+ 
+@Entity
+@Table(name = "Usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo_usuario")
+    private int codigoUsuario;
+    @Column
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private String email;
+    @Column
+    private String rol;
+    @Column
+    private int estado;
+ 
+    public Usuario() {
+    }
+ 
+    public Usuario(int codigoUsuario, String username, String password, String email, String rol, int estado) {
+        this.codigoUsuario = codigoUsuario;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.rol = rol;
+        this.estado = estado;
+    }
+ 
+    public int getEstado() {
+        return estado;
+    }
+ 
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+ 
+    public String getEmail() {
+        return email;
+    }
+ 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+ 
+    public String getRol() {
+        return rol;
+    }
+ 
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+ 
+    public String getUsername() {
+        return username;
+    }
+ 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+ 
+    public int getCodigoUsuario() {
+        return codigoUsuario;
+    }
+ 
+    public void setCodigoUsuario(int codigoUsuario) {
+        this.codigoUsuario = codigoUsuario;
+    }
+ 
+}
+ 
