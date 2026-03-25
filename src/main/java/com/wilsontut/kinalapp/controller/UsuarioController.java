d7896cdad39523597b11af2a8c7122662b64ad5b
wtut-2025353 <wtut-2025353@kinal.edu.gt>
2026-03-25 11:22:59 -0600

    feat: creación del controlador Usuario

---
+++ src/main/java/com/wilsontut/kinalapp/controller/UsuarioController.java
@@ -0,0 +1,85 @@
+package com.wilsontut.kinalapp.controller;
+
+import com.wilsontut.kinalapp.entity.Usuario;
+import com.wilsontut.kinalapp.service.IUsuarioService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final IUsuarioService usuarioService;
+
+    public UsuarioController(IUsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listar(){
+        List<Usuario> usuarios = usuarioService.listarTodos();
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Usuario> buscarPorCodigo(@PathVariable int codigo){
+        return usuarioService.buscarPorCodigo(codigo)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+    @PostMapping
+    public ResponseEntity<?> guardar(@RequestBody Usuario usuario){
+        try {
+            Usuario nuevoUsuario = usuarioService.guardar(usuario);
+            return new ResponseEntity<>(nuevoUsuario, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{codigo}")
+    public ResponseEntity<Void> eliminar(@PathVariable int codigo){
+        try{
+            if(!usuarioService.existePorCodigo(codigo)){
+                return ResponseEntity.notFound().build();
+            }
+            usuarioService.eliminar(codigo);
+            return ResponseEntity.noContent().build();
+        }
+        catch (RuntimeException e){
+            return  ResponseEntity.notFound().build();
+        }
+    }
+    @PutMapping("/{codigo}")
+    public ResponseEntity<?> actualizar(@PathVariable int codigo, @RequestBody Usuario usuario){
+        try{
+            if(!usuarioService.existePorCodigo(codigo)){
+                return ResponseEntity.notFound().build();
+            }
+            Usuario usuarioActualizado = usuarioService.actualizar(codigo,usuario);
+            return ResponseEntity.ok(usuarioActualizado);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }catch (RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @GetMapping( "/activos" )
+    public ResponseEntity<List<Usuario>> listarActivos(){
+        List<Usuario> activos = usuarioService.listarPorEstado(1);
+
+        return ResponseEntity.ok(activos);
+    }
+
+    @GetMapping("/inactivos")
+    public ResponseEntity<List<Usuario>>listarInactivos(){
+        List<Usuario> inactivos = usuarioService.listarPorEstado(0);
+
+        return ResponseEntity.ok(inactivos);
+    }
+}
