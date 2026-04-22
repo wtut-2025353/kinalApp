c12dfebcaa2e77a191d517926ab75bbb437ed414
Wilson <wtut-2025353@kinal.edu.gt>
2026-04-21 21:16:59 -0600

    feat: agregar query findByUsername en UsuarioRepository

--- src/main/java/com/wilsontut/kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/wilsontut/kinalapp/repository/UsuarioRepository.java
@@ -4,6 +4,9 @@ import com.wilsontut.kinalapp.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-    public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     List<Usuario> findByEstado(long estado);
+    Optional<Usuario> findByUsername(String username);
 }
